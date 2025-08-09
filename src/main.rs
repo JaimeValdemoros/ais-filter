29a4431f2f853e40dec39ac61dbc58da35ee30d7
Jaime Valdemoros <JaimeValdemoros@users.noreply.github.com>
2025-08-09 15:04:46 +0000

    Parse and pass through ais lines

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut parser = ais::AisParser::new();
+    for line in std::io::stdin().lines() {
+        let line = line?;
+        let parsed = parser.parse(line.as_bytes(), true)?;
+        eprintln!("{parsed:#?}");
+        println!("{line}");
+    }
+    Ok(())
 }
