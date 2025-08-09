462947a8d1eb02c88d2a4170b1dce99071347b6e
Jaime Valdemoros <JaimeValdemoros@users.noreply.github.com>
2025-08-09 17:42:19 +0000

    Track seen messages with bloom filter

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
+use std::time::{Duration, Instant};
+
+use ais::sentence::{AisFragments, AisSentence};
 use clap::Parser;
 use clap_duration::duration_range_value_parse;
 use duration_human::{DurationHuman, DurationHumanValidator};
+use fastbloom::BloomFilter;
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
@@ -15,17 +19,49 @@ struct Cli {
     quiet: bool,
 }
 
+struct Sample {
+    duration: Duration,
+    filter: BloomFilter,
+    next_tick: Instant,
+}
+
+impl Sample {
+    fn new(duration: &DurationHuman) -> Self {
+        let duration = duration.into();
+        Sample {
+            duration,
+            filter: BloomFilter::with_num_bits(1024).expected_items(128),
+            next_tick: Instant::now() + duration,
+        }
+    }
+
+    fn check(&mut self, sentence: &AisSentence) -> bool {
+        if self.next_tick < Instant::now() {
+            self.filter.clear();
+            self.next_tick += self.duration;
+        }
+        self.filter.insert(&sentence.data)
+    }
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     env_logger::init();
     let args = Cli::parse();
-    log::debug!("{:?}", args.sample);
+
+    let mut sample_cfg = args.sample.as_ref().map(Sample::new);
 
     let mut partial: Vec<String> = vec![];
     let mut parser = ais::AisParser::new();
+
     for line in std::io::stdin().lines() {
         let line = line?;
         match parser.parse(line.as_bytes(), true) {
-            Ok(ais::AisFragments::Complete(c)) => {
+            Ok(AisFragments::Complete(c)) => {
+                if sample_cfg.as_mut().map_or(false, |s| s.check(&c)) {
+                    log::debug!("Got duplicate message: {c:?}");
+                    partial.clear();
+                    continue;
+                }
                 if !args.quiet {
                     eprintln!("{c:#?}");
                 }
@@ -36,7 +72,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 }
                 println!("{line}");
             }
-            Ok(ais::AisFragments::Incomplete(_)) => {
+            Ok(AisFragments::Incomplete(_)) => {
                 partial.push(line);
             }
             Err(e) => {
